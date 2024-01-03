2695865c65501fee8579b4a778daf5344c463cd9
Mustafizur Rahman <sojibvikings@gmail.com>
2024-01-03 11:22:25 +0600

    camp details

--- APIComponents/CampListProcess.py
+++ APIComponents/CampListProcess.py
@@ -8,9 +8,15 @@ class CampList:
     @classmethod
     def get_camp_user(cls):
         camp_lists = []
-        camps = Camp.objects.exclude(name='public').values()
-        # for camp in camps:
-        #     connection.set_schema(camp['name'], True)
-        #     teachers = Teacher.objects.all().values()
-        #     connection.set_schema_to_public()
-        return camps
+        camps = Camp.objects.exclude(schema_name='public').values()
+        for camp in camps:
+            connection.set_schema(camp['schema_name'], True)
+            teachers = Teacher.objects.all().values()
+            students = Student.objects.all().values()
+            parents = Parent.objects.all().values()
+            camp['teachers'] = list(teachers)
+            camp['students'] = list(students)
+            camp['parents'] = list(parents)
+            camp_lists.append(camp)
+            connection.set_schema_to_public()
+        return camp_lists
